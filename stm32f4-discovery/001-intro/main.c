fe21a6f2736499af8e74cb482515110c0007dbe5
Kuba Sejdak <kuba.sejdak@gmail.com>
2017-10-07 11:19:48 +0200

    001: Add working UART example.

--- stm32f4-discovery/001-intro/main.c
+++ stm32f4-discovery/001-intro/main.c
@@ -10,28 +10,45 @@
 
 #include "stm32f4xx_gpio.h"
 #include "stm32f4xx_rcc.h"
+#include "stm32f4xx_usart.h"
 
-#define LED_RED     GPIO_Pin_14
-#define LED_BLUE    GPIO_Pin_15
+static void gpio_init()
+{
+    GPIO_InitTypeDef GPIO_InitStruct;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource10, GPIO_AF_UART4);
+    GPIO_InitStruct.GPIO_Pin = GPIO_Pin_10;
+    GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+    GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+static void uart_init()
+{
+    USART_InitTypeDef USART_InitStruct;
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4, ENABLE);
+
+    USART_InitStruct.USART_BaudRate = 115200;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStruct.USART_Mode = USART_Mode_Tx;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_Init(UART4, &USART_InitStruct);
+    USART_Cmd(UART4, ENABLE);
+}
 
 int main(int argc, char *argv[])
 {
-    // Enable peripheral clock for GPIOD.
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    gpio_init();
+    uart_init();
 
-    // Configure pins for red anb blue LEDs to output pushpull mode.
-    GPIO_InitTypeDef GPIO_InitStructure;
-    GPIO_InitStructure.GPIO_Pin = LED_RED | LED_BLUE;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
-    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-    while (1) {
-        GPIOD->BSRRL = LED_RED | LED_BLUE;
-        GPIOD->BSRRH = LED_RED | LED_BLUE;
-    }
+    USART_SendData(UART4, 'x');
 
     return 0;
 }
